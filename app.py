4361a3e2cf83bc1ee7ea74ec3110282b87b1ca00
Het Kothari <projectecom0@gmail.com>
2024-07-24 10:23:29 +0530

    created endpoints for login and signup pages

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from openai import OpenAI
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 from datetime import datetime
+from flask_bcrypt import Bcrypt
 
 conn = psycopg2.connect(
     database="gpt_prompt-responses",
@@ -23,48 +24,89 @@ client = OpenAI(api_key=api_key)
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:root@localhost:5432/gpt_prompt-responses'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 
-@app.route("/")
-def user_profiles():
-    return render_template("index.html")
+class UserCreds(db.Model):
+    __tablename__ = 'user_creds'
+    sNo = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), unique=True, nullable=False)
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
 
 
-def create_db_table(table_name):
-    class Table(db.Model):
-        __tablename__ = table_name
-        sNo = db.Column(db.Integer, primary_key=True)
-        prompt = db.Column(db.String(5000))
-        responses = db.Column(db.String(8000))
-        history = db.Column(db.JSON)
-        timestamp = db.Column(db.DateTime, default=datetime.now())
+class Table(db.Model):
+    __tablename__ = 'generic_table'
+    sNo = db.Column(db.Integer, primary_key=True)
+    prompt = db.Column(db.String(5000))
+    responses = db.Column(db.String(8000))
+    history = db.Column(db.JSON)
+    timestamp = db.Column(db.DateTime, default=datetime.now())
 
-        def __init__(self, prompt, responses, history, timestamp):
-            self.prompt = prompt
-            self.responses = responses
-            self.history = history
-            self.timestamp = timestamp
-
-    return Table
-    render_template('db.html')
+    def __init__(self, prompt, responses, history, timestamp):
+        self.prompt = prompt
+        self.responses = responses
+        self.history = history
+        self.timestamp = timestamp
 
 
-# @app.route("/submit", methods=["GET", "POST"])
-# def submit():
-#     if request.method == 'POST':
-#         prompt = request.form.get("prompt_data")
-#         responses = request.form.get("result")
-#         table = Table(prompt, responses)
-#         db.session.add(table)
-#         db.session.commit()
-#         return redirect(url_for('submit'))
-#     return render_template('submit.html')
+@app.route("/", methods=['POST', 'GET'])
+def signup_page():
+    if request.method == 'POST':
+        name = request.form.get("name")
+        email = request.form.get("email")
+        unhashed_password = request.form.get("password")
+        password = bcrypt.generate_password_hash(unhashed_password)
+        # password = request.form.get("password")
+        print(name, email, password)
+        cred_table = UserCreds(name=name, email=email, password=password)
+        db.session.add(cred_table)
+        db.session.commit()
+    return render_template('signup.html')
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login_page():
+    if request.method == 'POST':
+        name = request.form.get("name")
+        email = request.form.get("email")
+        # unhashed_password = request.form.get("password")
+        # password = bcrypt.generate_password_hash(unhashed_password)
+        # print(password)
+        password = request.form.get("password")
+        # cursor.execute('SELECT password FROM public.user_creds WHERE email = %s', (email,))
+        # result = cursor.fetchone()
+
+        email_list = []
+        cursor.execute('SELECT email from public.user_creds')
+        emails = cursor.fetchall()
+        for x in emails:
+            email_list.append(x[0])
+
+        pwd_list = []
+        cursor.execute('SELECT password from public.user_creds')
+        pwds = cursor.fetchall()
+        for x in pwds:
+            pwd_list.append(x[0])
+
+        if email in email_list:
+            if bcrypt.check_password_hash():
+            # if password in pwd_list:
+                return redirect('/user1')
+
+    return render_template('login.html')
 
 
 def create_endpoints(username, table_model):
-    # @app.route(f"/{username}", methods=['POST', 'GET'])
+    @app.route(f"/{username}", methods=['POST', 'GET'])
     def user_endpoint():
         if request.method == 'POST':
             prompt_data = request.form["prompt_data"]
@@ -106,69 +148,6 @@ def create_endpoints(username, table_model):
     app.route(f"/{username}", methods=['POST', 'GET'])(user_endpoint)
 
 
-# @app.route("/user2", methods=["GET", "POST"])
-# def user2():
-#     if request.method == 'POST':
-#         prompt_data = request.form["prompt_data"]
-#         response = client.chat.completions.create(
-#             model=model,
-#             messages=[
-#                 {'role': 'system', 'content': 'You are an assistant designed to extract key indicators and trading '
-#                                               'conditions from a given paragraph of information and generate a JSON'
-#                                               ' file in a specific format structure.'},
-#                 {'role': 'user', 'content': prompt_data}
-#             ],
-#             max_tokens=600,
-#             temperature=0.4
-#         )
-#         result = response.choices[0].message.content
-#
-#         table = Table(prompt=prompt_data, responses=result)
-#         db.session.add(table)
-#         db.session.commit()
-#
-#         return redirect(url_for('user2', result=result))
-#     result = request.args.get('result')
-#     return render_template('interface.html', result=result)
-#
-#
-# @app.route("/user3")
-# def user3():
-#     return "This is the page of user3"
-
-
-@app.route("/navigate_pages", methods=['POST'])
-def navigate_pages():
-    selected_users = request.form.get("users")
-    if selected_users == 'user1':
-        return redirect(url_for('user1'))
-    elif selected_users == 'user2':
-        return redirect(url_for('user2'))
-    elif selected_users == 'user3':
-        return redirect(url_for('user3'))
-    else:
-        return redirect(url_for('/'))
-
-
-# def show_database(username):
-#     table_name = f"{username}_data"
-#     with current_app.app_context():
-#         query = f"SELECT * FROM {table_name}"
-#         cursor.execute(query)
-#         result2 = cursor.fetchall()
-#         return render_template('interface.html', result=result2)
-
-
-user1Table = create_db_table('user1_data')
-user2Table = create_db_table('user2_data')
-user3Table = create_db_table('user3_data')
-
-
-create_endpoints('user1', user1Table)
-create_endpoints('user2', user2Table)
-create_endpoints('user3', user3Table)
-
-
 @app.route('/dbshow/<username>', methods=["POST", "GET"])
 def show_database(username):
     data_name = f'{username}_data'
@@ -184,13 +163,7 @@ def show_database(username):
     return render_template('db.html', output=output, username=username, timestamp=timestamp)
 
 
-# show_database('user1')
-# show_database('user2')
-# show_database('user3')
-
-
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-
